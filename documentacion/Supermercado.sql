708566a35b45e3c87f06e5b1ebe4bada60b558fb
Valenmoh <103288890+Valenmoh@users.noreply.github.com>
2022-06-21 20:30:09 -0300

    Add files via upload

---
+++ documentacion/Supermercado.sql
@@ -0,0 +1,34 @@
+create database supermercado;
+use supermercado;
+
+create table Usuario( 
+	Id_Usuario int not null,
+    Nombre varchar(20) not null,
+    Contraseña varchar(20) not null,
+    primary key(Id_Usuario)
+);
+
+create table Mercado(
+	Id_Mercado int not null,
+    Nombre varchar(20),
+    Descuentos float,
+    primary key (Id_Mercado)
+);
+
+create table Lista(
+	Id_Lista int not null,
+    Nombre varchar(10) not null,
+    Precio float,
+    primary key (Id_Lista),
+    foreign key (Id_Usuario) references Usuario(Id_Usuario),
+    foreign key (Id_Producto) references Producto(Id_Producto)
+);
+
+create table Producto(
+	Id_Producto int not null,
+    Nombre varchar(20),
+    Marca varchar(20),
+    Precio float,
+    primary key (Id_Producto),
+    foreign key (Id_Mercado) references Mercado(Id_Mercado)
+);
